83d92dadcc53cd9ece6291ed1f402ccb918b25a0
Oksana Sałyk <oksana.salyk@hpe.com>
2025-12-16 11:53:53 +0100

    obj: add footer ignoring for valgrind (#27)
    Signed-off-by: Oksana Salyk <oksana.salyk@hpe.com>

--- src/libpmemobj/memblock.c
+++ src/libpmemobj/memblock.c
@@ -1174,8 +1174,13 @@ huge_write_footer(struct chunk_header *hdr, uint32_t size_idx)
 	struct chunk_header f = *hdr;
 	f.type = CHUNK_TYPE_FOOTER;
 	f.size_idx = size_idx;
+
+	/*
+	 * no need to transact and persist,
+	 * footers are recreated in heap_populate_buckets
+	 */
+	VALGRIND_ADD_TO_GLOBAL_TX_IGNORE(hdr + size_idx - 1, sizeof(f));
 	*(hdr + size_idx - 1) = f;
-	/* no need to persist, footers are recreated in heap_populate_buckets */
 	VALGRIND_SET_CLEAN(hdr + size_idx - 1, sizeof(f));
 }
 
