69925cf455ef672c4cbdbdb13bef7ae581e67045
Liang Zhen <gnailzenh@gmail.com>
2026-04-24 22:09:55 +0800

    obj: recalculate curr_allocated on underflow (#37)
    * DAOS-18882 pmdk: rebuild heap_curr_allocated from on-media state
    
    rebuild heap_curr_allocated from on-media state if the counter has
    underflowed.
    
    Signed-off-by: Liang Zhen <gnailzenh@gmail.com>
    
    * obj: recalculate curr_allocated on underflow
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: fix copyrights
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: remove redundant comment
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: add unit test
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: fix Copyright
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: improve underflow detectio and...
    
    ... move recalculation to the getter.
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    Co-authored-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    
    * obj: restore the warning message
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: remove redundant header
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: fixes + updated unit test
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    * obj: fix comments in the unit test
    
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    
    ---------
    
    Signed-off-by: Liang Zhen <gnailzenh@gmail.com>
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>
    Co-authored-by: Jan Michalski <jan-marian.michalski@hpe.com>
    Co-authored-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

---
+++ src/test/obj_ctl_stats_curr_allocated_wa/obj_ctl_stats_curr_allocated_wa.c
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2026, Hewlett Packard Enterprise Development LP */
+
+/*
+ * obj_ctl_stats_curr_allocated_wa.c -- tests for curr_allocated WA
+ */
+
+#include "unittest.h"
+
+#include <../../libpmemobj/obj.h>
+
+#define LAYOUT "ctl"
+
+#define MB (1 << 20)
+
+/*
+ * set_curr_allocated -- set curr_allocated to a specific value
+ */
+static void
+set_curr_allocated(PMEMobjpool *pop, uint64_t value)
+{
+	struct pmemobjpool *ppop = (struct pmemobjpool *)pop;
+	uint64_t *curr_allocated = &ppop->stats_persistent.heap_curr_allocated;
+	*curr_allocated = value;
+	pmem_persist(curr_allocated, sizeof(*curr_allocated));
+}
+
+/*
+ * get_curr_allocated -- get the curr_allocated value
+ */
+static uint64_t
+get_curr_allocated(PMEMobjpool *pop)
+{
+	int ret;
+	uint64_t allocated;
+
+	ret = pmemobj_ctl_get(pop, "stats.heap.curr_allocated", &allocated);
+	UT_ASSERTeq(ret, 0);
+
+	return allocated;
+}
+
+/*
+ * huge_alloc -- make a huge allocation
+ */
+static void
+huge_alloc(PMEMobjpool *pop, PMEMoid *oid)
+{
+	int ret = pmemobj_alloc(pop, oid, 4 * MB, 0, NULL, NULL);
+	UT_ASSERTeq(ret, 0);
+}
+
+static void
+underflow(PMEMobjpool *pop, uint64_t *exp_allocated)
+{
+	UT_ASSERTne(exp_allocated, NULL);
+
+	/* Create a huge allocation and query the statistic */
+	PMEMoid oid;
+	huge_alloc(pop, &oid);
+
+	*exp_allocated = get_curr_allocated(pop);
+
+	/* Inject a smaller than expected value. */
+	set_curr_allocated(pop, *exp_allocated - 1);
+
+	/* Trigger underflow. */
+	pmemobj_free(&oid);
+}
+
+static void
+test(PMEMobjpool *pop)
+{
+	PMEMoid oid;
+	uint64_t exp_allocated;
+
+	/* 1. trigger underflow */
+	underflow(pop, &exp_allocated);
+
+	/* Check the statistic is updated correctly. */
+	UT_ASSERTeq(get_curr_allocated(pop), 0);
+
+	/* 2. trigger underflow and attempt crossing zero again */
+	underflow(pop, &exp_allocated);
+	huge_alloc(pop, &oid);
+
+	/* Check the statistic is updated correctly. */
+	UT_ASSERTeq(get_curr_allocated(pop), exp_allocated);
+}
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "obj_ctl_stats_curr_allocated_wa");
+
+	if (argc != 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	const char *path = argv[1];
+
+	PMEMobjpool *pop;
+	pop = pmemobj_create(path, LAYOUT, PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+	UT_ASSERTne(pop, NULL);
+
+	int enabled = 1;
+	int ret;
+
+	/* Enable stats. */
+	ret = pmemobj_ctl_set(pop, "stats.enabled", &enabled);
+	UT_ASSERTeq(ret, 0);
+
+	test(pop);
+
+	pmemobj_close(pop);
+
+	DONE(NULL);
+}
